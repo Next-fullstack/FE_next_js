bdd9b13e753f3e655e079add6e7fff96d967d428
qibal <iqbalherlambang.39@gmail.com>
2025-01-30 13:06:08 +0700

    update regsiter

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
                 <div className="w-full max-w-md space-y-6">
                     <div className="space-y-2 text-center">
                         <h1 className="text-3xl font-bold tracking-tight">Daftar</h1>
-                        <p className="text-muted-foreground">Sudah punya akun? <Link href={'/login'} className="font-semibold hover:underline hover:underline-offset-1">daftar</Link></p>
+                        <p className="text-muted-foreground">Sudah punya akun? <Link href={'/login'} className="font-semibold hover:underline hover:underline-offset-1">Masuk</Link></p>
                     </div>
                     <RegisterForm />
                 </div>
